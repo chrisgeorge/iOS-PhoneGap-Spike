b43c71881a3935f7829b63dfdf683e2647d3cbaa
chrisgeorge <Thoughtworker@dhcp-215.internal.democracynow.org>
2013-06-14 15:27:35 -0400

    restyling for better look and feel

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,9 @@
 $(document).ready( function() {
 
   $.get('http://staging1.democracynow.org/api/1/story/13648?callback=?', function(story) {
-  	$("#story-date").html(story.publicationDate);
+  	$("#story-date").html(story.publicationDate.substring(0,10));
     $("#story-title").html(story.title);
-  	$("#story-body").html(story.transcript);
+    $("#story-summary").html(story.summary);
+  	$("#story-transcript").html(story.transcript);
   });
 });
