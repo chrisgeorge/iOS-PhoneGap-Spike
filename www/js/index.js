0437819c3001d55a944f232443b2d539966c7ae8
chrisgeorge <Thoughtworker@dhcp-215.internal.democracynow.org>
2013-06-17 12:10:50 -0400

    cg - #2018 - removing current project after having better understanding of cordova / phonegap setup

--- www/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function() {
-
-  $.get('http://staging1.democracynow.org/api/1/story/13648?callback=?', function(story) {
-  	$("#story-date").html(story.publicationDate.substring(0,10));
-    $("#story-title").html(story.title);
-    $("#story-summary").html(story.summary);
-  	$("#story-transcript").html(story.transcript);
-  });
-});
