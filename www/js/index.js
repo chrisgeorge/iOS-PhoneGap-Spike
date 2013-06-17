235b4221e2f4e7c9443331eac73c20250ee73d88
chrisgeorge <Thoughtworker@dhcp-215.internal.democracynow.org>
2013-06-17 15:34:45 -0400

    cg - #2018 - switched around audio a bit ... now plays on device, but still fades out when screen locks (even though it shouldn't by default)

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,28 @@
-var myAudio = function(url){
-    var myaudio = new Audio(url);
-    myaudio.id = 'playerMyAudio';
-
-    this.pauseStream = function(){
-        try{
-            myaudio.pause();
-        } catch (e){
-            alert('problem pausing audio');
-        }
-    }
-
-    this.playStream = function() {
-      try {
-        myaudio.play();
-      } catch (e) {
-        alert('no audio support!');
-      }
-    }
-}
-
-registerAudioEvents = function(audio){
-    $("#story-audio a").click(function() {
-        audio.playStream();
-        return false;
+registerAudioEvents = function(url){
+    var audio = new Media(url,
+        function() {
+            console.log("playAudio():Audio Success");
+        },
+        function(err) {
+            console.log("playAudio():Audio Error: "+err);
+        });
+    
+    $("#story-audio").click(function() {
+        audio.play({playAudioWhenScreenIsLocked : true});
      });
 
-    $("#story-audio-pause a").click(function() {
-        audio.pauseStream();
-        return false;
+    $("#story-audio-pause").click(function() {
+        audio.pause();
     });
 }
 
 $(document).ready( function() {
-
   $.get('http://staging1.democracynow.org/api/1/story/13648', function(story) {
   	$("#story-date").html(story.publicationDate.substring(0,10));
     $("#story-title").html(story.title);
   	$("#story-summary").html(story.summary);
   	$("#story-transcript").html(story.transcript);
 
-    audioSource = story.media[0].src;
-    audio = new myAudio(audioSource);
-    registerAudioEvents(audio);
+    registerAudioEvents(story.media[0].src);
   });
 });
